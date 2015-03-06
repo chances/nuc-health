22857df816dacfc9fa9a97750257d091559b34d4
johnschroeder <john.schroeder4@gmail.com>
2015-03-06 14:11:48 -0800

    Initial test commit with modifications to main

--- src/main/java/nucchallenge/App.java
+++ src/main/java/nucchallenge/App.java
@@ -7,6 +7,6 @@ package nucchallenge;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Nerds!" );
     }
 }
